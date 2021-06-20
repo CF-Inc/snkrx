d1a02250023645762d5eef210c657b6591bd240d
wSedlacek <wsedlacekc@gmail.com>
2021-06-20 12:07:03 -0700

    chore(repo): initialize code quality ci/cd
    This configures GitHub Actions for litning and testing

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   projects: [
+    '<rootDir>/apps/snkrx',
     '<rootDir>/libs/shared/util-dom-events',
     '<rootDir>/libs/shared/util-context-menu',
     '<rootDir>/libs/shared/ui-icons',
     '<rootDir>/libs/shared/util-fp',
-    '<rootDir>/apps/snkrx',
   ],
 };
